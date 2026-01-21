ec0a930a2f169dc25c5b40ebb6b93f3bee5e7196
dimazmlbig-dev <dimazmlbig@gmail.com>
2026-01-21 20:02:06 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -8,18 +8,10 @@ const State = {
   addBones(amount) {
     if (!Number.isFinite(amount)) return;
     this.data.bones += amount;
-    UI.updateBalance();
   },
 
-  spendEnergy(amount) {
-    if (this.data.energy < amount) return false;
-    this.data.energy -= amount;
-    UI.updateEnergy();
-    return true;
-  },
-
-  save() {
-    StorageManager.saveState(this.data);
+  incrementTaps() {
+    this.data.totalTaps = (this.data.totalTaps || 0) + 1;
   }
 };
 

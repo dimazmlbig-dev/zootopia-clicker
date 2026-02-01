c0d1b9ac0f1df72e2c11571091ef8a629c124310
dimazmlbig-dev <dimazmlbig@gmail.com>
2026-02-01 21:11:54 +0300

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,18 +1,4 @@
-(function(){
-  window.WalletScreen = {
-    html(){
-      const g = window.STATE.game;
-      return `
-        <div class="centerScreen">
-          <div class="card">
-            <div style="font-weight:900;font-size:18px;margin-bottom:10px;color:rgba(255,255,255,.92)">Кошелёк</div>
-            <div style="color:rgba(255,255,255,.75);line-height:1.45">
-              Баланс в игре: <b style="color:rgba(255,255,255,.92)">${g.balance} $ZOO</b><br><br>
-              Дальше подключим TonConnect и показ TON Balance через Worker.
-            </div>
-          </div>
-        </div>
-      `;
-    }
-  };
+window.Wallet = (() => {
+  function bind(){ /* later */ }
+  return { bind };
 })();
